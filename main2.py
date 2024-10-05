ea3637341d2ee623d386bf938e3b4b96503834b8
yayahpaujiah <yayahpaujiah39@gmail.com>
2024-10-05 15:05:51 +0700

    add repository

---
+++ main2.py
@@ -0,0 +1,11 @@
+nama_lengkap = "Nama Yayah Paujiah"
+nim = 123
+prodi = "Teknik Informatika"
+nilai = 8.5
+lulus_ujian = True
+
+print(f"Nama: {nama_lengkap}, NIM: {nim}, Prodi: {prodi}, Nilai: {nilai}, Lulus Ujian: {lulus_ujian}")
+
+
+output = "Nama: " + nama_lengkap + "\nNIM: " + str(nim) + "\nProdi: " + prodi + "\nNilai: " + str(nilai) + "\nStatus Ujian: " + str(lulusan_ujian) print(output)
+print(output)
---
+++ main3.py
@@ -0,0 +1,37 @@
+#Mendefinisikan variabel
+angka1 = 10
+angka2 = 5
+angka3 = 2
+
+# #Operasi penjumlahan
+# hasil_penjumlahan = angka1 + angka2 + angka3
+# print("Hasil penjumlahan:", hasil_penjumlahan)
+
+# #Operasi pengurangan
+# hasil_pengurangan = angka1 - angka2 - angka3
+# print("Hasil pengurangan:", hasil_pengurangan)
+
+# #Operasi perkalian
+# hasil_perkalian = angka1 * angka2 * angka3
+# print("Hasil perkalian:", hasil_perkalian)
+
+# #Operasi pembagian
+# hasil_pembagian = angka1 / angka2 / angka3
+# print("Hasil pembagian:", hasil_pembagian)
+
+# #Operasi pemangkatan
+# hasil_pemangkatan = angka1 ** angka2 ** angka3
+# print("Hasil pemangkatan:", hasil_pemangkatan)
+# print(angka1 == angka2)
+# print(angka1 != angka2)
+# print(angka1 > angka2)
+# print(angka1 < angka2)
+# print(angka1 >= angka2)
+# print(angka1 <= angka1)
+
+nilai1 = 15
+nilai2 = 10
+nilai3 = 20
+print(nilai2 == nilai3)
+print(nilai1 >= nilai3)
+print(nilai1 < nilai3)
